170e075efc7395929e3ee722a9ec83a862223555
Roy Hashimoto <roy@shoestringresearch.com>
2019-07-30 14:38:58 -0700

    Add minimal OpenGL ES setup.

--- app/src/main/java/com/example/glgeneratemipmap/MainActivity.kt
+++ app/src/main/java/com/example/glgeneratemipmap/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.glgeneratemipmap
 
+import android.opengl.GLES20.*
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    surfaceView.setEGLContextClientVersion(2)
+    surfaceView.setRenderer(Renderer())
+    surfaceView.preserveEGLContextOnPause = true
+  }
+}
+
+class Renderer : GLSurfaceView.Renderer {
+  override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
+  }
+
+  override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
+    glViewport(0, 0, width, height)
+  }
+
+  override fun onDrawFrame(p0: GL10?) {
+    glClearColor(1f, 0f, 0f, 1f)
+    glClear(GL_COLOR_BUFFER_BIT)
   }
 }
